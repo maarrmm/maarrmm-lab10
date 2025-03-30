7cec5042bdb50f74a50b9a44c6946b6ce390a5d0
Mariam <mariam.eldigwy@gwmail.gwu.edu>
2025-03-30 16:10:36 -0400

    progress

--- WordCounter.java
+++ WordCounter.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.regex.*;
 import java.util.Scanner;
@@ -10,34 +12,81 @@ public class WordCounter{
         Matcher matcher = pattern.matcher(text);
 
         int totalCount = 0;
+        int stopCount = 0;
+        boolean found = false;
         while(matcher.find()){
             totalCount++;
+            if(!found){
+                stopCount++;
+            }
+            if(stopWord != null && matcher.equals(stopWord)){
+                found = true;
+            }
+            
         }
         
         if(totalCount < 5){
-            throw new TooSmallText(totalCount);
+            throw new TooSmallText("Only Found " + totalCount + " words.");
         }
 
         if(stopWord == null){
             return totalCount;
-        } else {
-            boolean found = false;
-            if(matcher.equals(stopWord)){
-                found = true;
-                break;
-            }
-            if(!found){
-                throw new InvalidStopwordException("Couldn't find stopword: " + stopWord);
-            }
         }
+
+        if(!found){
+            throw new InvalidStopwordException("Couldn't find stopword: " + stopWord);
+        }  
+
+        return stopCount;
     }
 
     public static String processFile(String path){
 
-        Scanner fileScanner = null;
+        Scanner input = new Scanner(System.in);
         File file = new File(path);
+        
+        while(!file.exists() || !file.canRead()){
+            System.out.println("Failed to open.");
+            path = input.nextLine();
+            file = new File(path);                                                                                          
+        }
 
+        StringBuffer sb = new StringBuffer();
 
+        try{
+            Scanner fileScanner = new Scanner(file);
+            while(fileScanner.hasNextLine()){
+                sb.append(fileScanner.nextLine()).append(" ");
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e){
+            System.out.println("could not open");
+        }
+        
+        return sb;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StringBuffer text = new StringBuffer();
+        String stopWord = null;
+
+        while(!input.equals(1) && !input.equals(2)){
+            System.out.println("Enter 1 or 2");
+        }
+
+        if(args.length > 1){
+            stopWord = args[1];
+        }
+
+        if(input.equals(1)){
+            try {
+                text = processFile(stopWord);
+            } catch (EmptyFileException e) {
+                System.out.println(e);
+                text = new StringBuffer("");
+            }
+        }
     }
 
 
